fcd783b2367a301658db5e976dc08c16600f67ac
mengfly <www.wangpengfei99@gmail.com>
2017-09-17 12:21:50 +0800

    :palm_tree:

--- python/moudle/test4.py
+++ python/moudle/test4.py
@@ -1,36 +1,4 @@
 
-print sorted([25,78,93,32], lambda x, y : -cmp(x, y))
-print filter(lambda s : s and len(s.strip()) > 0, ['','f'])
-
-# 装饰器decorator
-# 装饰器可以极大地简化代码，避免每个函数编写重复性代码
-
-# 编写无参数decorator
-# Python的decorator本质上就是一个高阶函数，他接受一个函数作为参数，然后返回一个新函数
-# 使用decorator用Python提供的@ 语法，就可以避免写f = decorator(f)这样的语句
-# 例子（为函数提供打印log的功能）
-def log(f) :
-	def fn(x) :
-		print 'call' + f.__name__ + '()...'
-		return f(x)
-	return fn
-@log
-def factorial(n) :
-	return reduce(lambda x, y : x * y, range(1, n + 1))
-print factorial(10)     #callfactorial()...3628800
-#log()为函数添加打印log的功能，使用@log就相当于f = log(f)
-
-# 要让@log可以适应于任何参数定义的函数，可以使用Python的*args和**kw，保证任意个参数总是能正常调用
-def log(f) :
-	def fn(*args, **kw) :
-		print 'call' + f.__name__ + '()...'
-		return f(*args, **kw)
-	return fn
-@log
-def add(x, y) :
-        return x + y
-print add(2, 4)         #calladd()...6
-
 # 编写带参数的decorator
 # 上面定义的log函数打印的log是定义好的，函数名()...,如果想要自定义log语句，可以为decorator定义参数
 # 例子：
