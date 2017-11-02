01e5438e18dd64b52201b48e76f988ef14a0a39c
mengfly <www.wangpengfei99@gmail.com>
2017-11-02 19:19:52 +0800

    :deciduous_tree: 装饰器模式

--- python/db/account_money.py
+++ python/db/account_money.py
@@ -20,7 +20,7 @@ class TransferMoney(object):
         try:
             sql = "select * from account where acctid=%s" %acctid
             cursor.execute(sql)
-            print "check_acct_available" + sql
+            print("check_acct_available" + sql)
             rs = cursor.fetchall()
             if len(rs) != 1 :
                 raise Exception("账号%s不存在"%acctid)
@@ -33,7 +33,7 @@ class TransferMoney(object):
         try:
             sql = "select * from account where acctid=%s and money >= %s" %(acctid,money)
             cursor.execute(sql)
-            print "has_enough_money" + sql
+            print("has_enough_money" + sql)
             rs = cursor.fetchall()
             if len(rs) != 1 :
                 raise Exception("账号%s余额不足"%acctid)
@@ -47,11 +47,11 @@ class TransferMoney(object):
         try:
             sql = "update account set money=money-%s where acctid=%s" %(money,acctid)
             cursor.execute(sql)
-            print "reduce_money" + sql
+            print("reduce_money" + sql)
             if cursor.rowcount != 1 :
                 raise Exception("账号%s减款失败"%acctid)
             else :
-                print "账号%s付款%s成功"%(acctid, money)
+                print("账号%s付款%s成功"%(acctid, money))
         finally:
             cursor.close()
     
@@ -61,11 +61,11 @@ class TransferMoney(object):
         try:
             sql = "update account set money=money + %s where acctid=%s" %(money,acctid)
             cursor.execute(sql)
-            print "reduce_money" + sql
+            print("reduce_money" + sql)
             if cursor.rowcount != 1 :
                 raise Exception("账号%s加款失败"%acctid)
             else :
-                print "账号%s收款%s成功"%(acctid, money)
+                print("账号%s收款%s成功"%(acctid, money))
         finally:
             cursor.close()
         
@@ -78,7 +78,7 @@ class TransferMoney(object):
             self.reduce_money(source_acctid, money)
             self.add_money(target_acctid, money)
             self.conn.commit()
-            print "转账成功"
+            print("转账成功")
         except Exception as e:
             self.conn.rollback()
             raise e # 把异常重新抛出
@@ -98,6 +98,6 @@ if __name__ == "__main__":
     try :
         tr_money.transfer(source_acctid, target_acctid, money)
     except Exception as e:
-        print "出现问题" + str(e)
+        print("出现问题" + str(e))
     finally :
         conn.close()
